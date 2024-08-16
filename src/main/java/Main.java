ef68e6a40f6bd234c66df14f39369ed125625aeb
assk <heyantariksha@gmail.com>
2024-08-16 09:24:39 +0530

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hey ganiga!");
         System.out.println("adsf");
+        System.out.println("adsf");
     }
 }
